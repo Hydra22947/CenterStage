091633a0edc0b29aede047ed51f8099b7bf27682
Elior Yousefi <elioryous@gmail.com>
2023-11-13 15:51:04 +0200

    Create IntakeTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/IntakeTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp(name="Intake Test", group="tests")
+@Config
+public class IntakeTest extends LinearOpMode {
+
+    private CRServo intake = null;
+
+    public static double power = 0.1;
+
+    @Override
+    public void runOpMode() {
+        intake  = hardwareMap.get(CRServo.class, "sI");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            intake.setPower(power);
+        }
+    }
+}
