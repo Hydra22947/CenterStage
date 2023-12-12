f913235139b5973e0185326279e71c12042024ff
Elior Yousefi <elioryous@gmail.com>
2023-12-12 18:08:58 +0200

    Update TestAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/TestAuto.java
@@ -1,108 +1,105 @@
 package org.firstinspires.ftc.teamcode.roadrunner;
 
-import androidx.core.math.MathUtils;
-
-import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.controller.PIDFController;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.RobotHardware;
 import org.firstinspires.ftc.teamcode.util.Pose;
 
-
-@Autonomous(name="Test Auto", group="tests")
 @Config
-public class TestAuto extends LinearOpMode {
+public class PositionCommand implements LinearOpMode {
+    Pose targetPose;
 
-    public static double ALLOWED_TRANSLATIONAL_ERROR = 0.25;
-    public static double ALLOWED_HEADING_ERROR = Math.toRadians(1);
+    public static double xP = 0.0385;
+    public static double xD = 0.005;
+
+    public static double yP = 0.0385;
+    public static double yD = 0.005;
+
+    public static double hP = 0.75;
+    public static double hD = 0.02;
+
+    public static double kStatic = 0.05;
+
+    public static PIDFController xController = new PIDFController(xP, 0.0, xD, 0);
+    public static PIDFController yController = new PIDFController(yP, 0.0, yD, 0);
+    public static PIDFController hController = new PIDFController(hP, 0.0, hD, 0);
+
+    public static double ALLOWED_TRANSLATIONAL_ERROR = 1;
+    public static double ALLOWED_HEADING_ERROR = 0.03;
 
     private RobotHardware robot = RobotHardware.getInstance();
 
+    private ElapsedTime timer;
+    private ElapsedTime stable;
     MecanumDrive drive;
-    public static double xP = 0.04, xD = 0.05, xF = 0;
-
-    public static double yP = 0.04,  yD = 0.05, yF = 0;
-
-    public static double hP = 0.6, hD = 0.3, hF = 0;
-
-    public static double x = 0, y = 10, h = 0;
-
-    PIDFController xController = new PIDFController(xP, 0, xD, xF);
-    PIDFController yController = new PIDFController(yP, 0, yD, yF);
-    PIDFController hController = new PIDFController(hP, 0, hD, hF);
-
-    public static double maxPower = 1;
-    public static double maxHeading = .5;
-    private double v;
 
     @Override
     public void runOpMode() {
-        CommandScheduler.getInstance().reset();
-        telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry());
         robot.init(hardwareMap, telemetry);
 
         drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0), robot);
 
         waitForStart();
 
-        while (opModeIsActive()) {
-            robot.loopVoltage(hardwareMap);
-            drive.updatePoseEstimate();
+        execute();
+    }
 
-            goToPosition(new Pose(drive.pose.position.x, drive.pose.position.y, drive.pose.heading.log()), new Pose(x,y,h));
-            telemetry.addData("x pos", drive.pose.position.x);
-            telemetry.addData("y pos", drive.pose.position.y);
-            telemetry.addData("heading pos", Math.toDegrees(drive.pose.heading.log()));
-            telemetry.update();
+    /**
+     *
+     */
+
+    public void execute() {
+        if (timer == null) timer = new ElapsedTime();
+        if (stable == null) stable = new ElapsedTime();
+
+        Pose robotPose = new Pose(drive.pose.position.x, drive.pose.position.y, drive.pose.heading.log());
+
+        Pose powers = getPower(robotPose);
+        setPowers(powers);
+    }
+
+    public boolean isFinished() {
+        Pose robotPose = new Pose(drive.pose.position.x, drive.pose.position.y, drive.pose.heading.log());
+
+        Pose delta = targetPose.subtract(robotPose);
+
+        if (delta.toVec2D().magnitude() > ALLOWED_TRANSLATIONAL_ERROR
+                || Math.abs(delta.heading) > ALLOWED_HEADING_ERROR) {
+            stable.reset();
         }
+
+        return timer.milliseconds() > 5000 || stable.milliseconds() > 150;
     }
 
-//    public boolean isFinished() {
-//        Pose error = targetPose.subtract(localizer.getPos());
-//
-//        boolean reached = ((Math.hypot(error.x, error.y) < ALLOWED_TRANSLATIONAL_ERROR) && (Math.abs(error.heading) < ALLOWED_HEADING_ERROR));
-//
-//        if (reached && delayTimer == null) {
-//            delayTimer = new ElapsedTime();
-//        }
-//        if (!reached && delayTimer != null) {
-//            delayTimer.reset();
-//        }
-//
-//        boolean delayed = delayTimer != null && delayTimer.milliseconds() > delay;
-//        return (deadTimer.milliseconds() > ms) || delayed;
-//    }
+    public Pose getPower(Pose robotPose) {
+        Pose delta = targetPose.subtract(robotPose);
 
+        double xPower = xController.calculate(robotPose.x, targetPose.x);
+        double yPower = yController.calculate(robotPose.y, targetPose.y);
+        double hPower = -hController.calculate(0, delta.heading);
 
-    private static Pose relDistanceToTarget(Pose robot, Pose target) {
-        return target.subtract(robot);
+        double x_rotated = xPower * Math.cos(robotPose.heading) - yPower * Math.sin(robotPose.heading);
+        double y_rotated = xPower * Math.sin(robotPose.heading) + yPower * Math.cos(robotPose.heading);
+
+        if (Math.abs(x_rotated) < 0.01) x_rotated = 0;
+        else x_rotated += kStatic * Math.signum(x_rotated);
+        if (Math.abs(y_rotated) < 0.01) y_rotated = 0;
+        else y_rotated += kStatic * Math.signum(y_rotated);
+        if (Math.abs(hPower) < 0.01) hPower = 0;
+        else hPower += kStatic * Math.signum(hPower);
+
+        return new Pose((y_rotated / robot.getVoltage() * 12.5) *1.6, x_rotated / robot.getVoltage() * 12.5, hPower / robot.getVoltage() * 12.5);
     }
 
-    public Pose goToPosition(Pose robotPose, Pose targetPose) {
-        Pose deltaPose = relDistanceToTarget(robotPose, targetPose);
-        Pose powers = new Pose(
-                xController.calculate(0, deltaPose.x),
-                yController.calculate(0, deltaPose.y),
-                hController.calculate(0, deltaPose.heading)
-        );
-        double x_rotated = powers.x * Math.cos(robotPose.heading) - powers.y * Math.sin(robotPose.heading);
-        double y_rotated = powers.x * Math.sin(robotPose.heading) + powers.y * Math.cos(robotPose.heading);
-        double x_power = -x_rotated < -maxPower ? -maxPower :
-                Math.min(-x_rotated, maxPower);
-        double y_power = -y_rotated < -maxPower ? -maxPower :
-                Math.min(-y_rotated, maxPower);
-        double heading_power = MathUtils.clamp(powers.heading, -maxHeading, maxHeading);
-
-        if(Math.abs(x_power) < 0.01) x_power = 0;
-        if(Math.abs(y_power) < 0.01) y_power = 0;
-
-        return new Pose(-y_power / robot.getVoltage() * 12, x_power / robot.getVoltage() * 12, -heading_power / robot.getVoltage() * 12);
+    void setPowers(Pose powers)
+    {
+        robot.dtFrontLeftMotor.setPower(powers.x + powers.heading + powers.y);
+        robot.dtBackLeftMotor.setPower(powers.x + powers.heading - powers.y);
+        robot.dtFrontRightMotor.setPower(powers.x  - powers.heading - powers.y);
+        robot.dtBackRightMotor.setPower(powers.x  - powers.heading + powers.y);
     }
 }
