56f5992265f22dba05073873811ef93eb600fafb
Elior Yousefi <elioryous@gmail.com>
2024-03-12 14:28:44 +0200

    meep meep for mac

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -279,7 +279,7 @@ public final class MecanumDrive {
 
             if ((t >= timeTrajectory.duration && error.position.norm() < 1
                     && robotVelRobot.linearVel.norm() < 0.5 && error.heading.log() < .5)
-                    || t >= timeTrajectory.duration + 2) {
+                    || t >= timeTrajectory.duration + 1) {
                 leftFront.setPower(0);
                 leftBack.setPower(0);
                 rightBack.setPower(0);
